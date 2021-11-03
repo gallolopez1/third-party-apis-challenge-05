200d3a2ab592d04c10450fb823c7d4e383d46f05
Rooster <gallolopez1@hotmail.com>
2021-11-03 15:34:53 -0400

    Add comments to js script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,13 @@
 function updateTimeSlotItem() {
     // assign classes (past /present /future)
     $('.hour').each(function() {
+        // select string within class hour
         var hours = $(this).text().trim();
+        // convert to hour object using momentjs
         var timeObject = moment(hours, 'LT');
+        // convert hours to numeric value, round using math.floor and find the difference between current time using moment()
         var duration = Math.floor(moment.duration(moment().diff(timeObject)).asHours());
+        // compare the duration against the current hour to see which class gets applied to it
         if (duration > 0) {
             $(this).next('.description').addClass('past');
         } else if (duration === 0) {
@@ -14,6 +18,7 @@ function updateTimeSlotItem() {
     });
     // read relevant local storage data (about the relevant hour slot)
     $('.time-block').each(function() {
+        // select current time to load items by using j query chicldre()
         var timeId = $(this).attr('id');
         $(this).children('.description').text(localStorage.getItem(timeId));
     });
@@ -22,6 +27,7 @@ function updateTimeSlotItem() {
 
 function handleSave(event) {
 
+    // save to local storage time value by id and text with class of description
     var hour = $(event.target).closest('.time-block').attr('id');
     var value = $(event.target).siblings('.description').val().trim();
     localStorage.setItem(hour, value);
