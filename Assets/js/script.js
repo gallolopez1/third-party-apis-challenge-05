0b8dd497ef2e8156682533fd25d4ade300381ffa
Rooster <gallolopez1@hotmail.com>
2021-11-03 15:27:30 -0400

    Update time and compare against moment to add class of past, present and future

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,16 +3,26 @@ function updateTimeSlotItem() {
     $('.hour').each(function() {
         var hours = $(this).text().trim();
         var timeObject = moment(hours, 'LT');
-        console.log(timeObject);
+        var duration = Math.floor(moment.duration(moment().diff(timeObject)).asHours());
+        if (duration > 0) {
+            $(this).next('.description').addClass('past');
+        } else if (duration === 0) {
+            $(this).next('.description').addClass('present');
+        } else {
+            $(this).next('.description').addClass('future');
+        }
     });
     // read relevant local storage data (about the relevant hour slot)
+    $('.time-block').each(function() {
+        var timeId = $(this).attr('id');
+        $(this).children('.description').text(localStorage.getItem(timeId));
+    });
 
 };
 
 function handleSave(event) {
 
     var hour = $(event.target).closest('.time-block').attr('id');
-    hour - 8
     var value = $(event.target).siblings('.description').val().trim();
     localStorage.setItem(hour, value);
 
